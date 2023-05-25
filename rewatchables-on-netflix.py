4e45136612fa1cc32871d55e2d62cb2b9da8d18c
Ben Rogers <benson8@gmail.com>
2023-05-25 18:27:19 -0500

    Used yapf to reformat python code

--- rewatchables-on-netflix.py
+++ rewatchables-on-netflix.py
@@ -26,24 +26,25 @@ with open(tmdb_id_csv_file, 'r') as csv_file:
     next(csv_file)
     reader = csv.reader(csv_file)
     for row in reader:
-       # grab the movie ID from the CSV file
-       try:
-           movie_id = row[2]
-       except:
-           print(f"'{row[0]}' is the last show that was being processed")
-       # Get the watch provider information for the movie
-       try:
-           watch_providers = movie.watch_providers(movie_id)
-       except:
-           time.sleep(15)
-           print(f"'{row[0]}' is the last show that was being processed")
-           watch_providers = movie.watch_providers(movie_id)
+        # grab the movie ID from the CSV file
+        try:
+            movie_id = row[2]
+        except:
+            print(f"'{row[0]}' is the last show that was being processed")
+        # Get the watch provider information for the movie
+        try:
+            watch_providers = movie.watch_providers(movie_id)
+        except:
+            time.sleep(15)
+            print(f"'{row[0]}' is the last show that was being processed")
+            watch_providers = movie.watch_providers(movie_id)
 
-       # Check if watch provider information is available for the US market
-       if 'US' in watch_providers['results'] and 'flatrate' in watch_providers['results']['US']:
-          us_providers = watch_providers['results']['US']['flatrate']
-          for provider in us_providers:
-             # Final check for Netflix availability
-             if provider['provider_name'] == "Netflix":
-                print(f"'{row[0]}' is available on Netflix in the US market") 
-
+        # Check if watch provider information is available for the US market
+        if 'US' in watch_providers[
+                'results'] and 'flatrate' in watch_providers['results']['US']:
+            us_providers = watch_providers['results']['US']['flatrate']
+            for provider in us_providers:
+                # Final check for Netflix availability
+                if provider['provider_name'] == "Netflix":
+                    print(
+                        f"'{row[0]}' is available on Netflix in the US market")
